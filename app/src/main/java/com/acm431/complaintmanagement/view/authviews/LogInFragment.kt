ba6de429df04b2bb9a38b89949b08c8641d4bf93
Arda Demirel <yademirel21@gmail.com>
2022-12-20 19:31:24 +0300

    Login&Register Connected to each other.

--- app/src/main/java/com/acm431/complaintmanagement/view/authviews/LogInFragment.kt
+++ app/src/main/java/com/acm431/complaintmanagement/view/authviews/LogInFragment.kt
@@ -1,9 +1,37 @@
 package com.acm431.complaintmanagement.view.authviews
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.acm431.complaintmanagement.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LogInFragment : Fragment(R.layout.fragment_login){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        register_button_login.setOnClickListener {
+            val action = LogInFragmentDirections.actionLogInFragmentToSignUpFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login,container,false)
+    }
 
 
 
--- app/src/main/java/com/acm431/complaintmanagement/view/authviews/SignUpFragment.kt
+++ app/src/main/java/com/acm431/complaintmanagement/view/authviews/SignUpFragment.kt
@@ -1,7 +1,37 @@
 package com.acm431.complaintmanagement.view.authviews
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.acm431.complaintmanagement.R
+import kotlinx.android.synthetic.main.fragment_signup.*
 
 class SignUpFragment : Fragment(R.layout.fragment_signup) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        tv_already_button.setOnClickListener {
+            val action = SignUpFragmentDirections.actionSignUpFragmentToLogInFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_signup,container,false)
+    }
+
+
 }
